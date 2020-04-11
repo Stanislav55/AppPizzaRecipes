025155724b1c693b98acc0cef36f5ea8bd95e8ed
Stanislav55 <info7762@gmail.com>
2020-04-12 01:01:39 +0600

    Part_4_Finish

--- app/src/main/java/com/example/pizzarecipes/PizzaRecipeItem.java
+++ app/src/main/java/com/example/pizzarecipes/PizzaRecipeItem.java
@@ -12,6 +12,7 @@ public class PizzaRecipeItem {
         this.imageResource = imageResource;
         this.title = title;
         this.description = description;
+        this.recipe = recipe;
     }
 
     public int getImageResource() {
